9679f9aef6d3b9755d34e5f33c30c97cca5c420e
Gregory Monroe <36177342+gnjmonroe@users.noreply.github.com>
2022-08-17 10:57:55 +0900

    Redesigned park nodes and labels. Remapped park-streams to work with new number of park nodes. Refactored javascript for simplicity.

--- web/script.js
+++ web/script.js
@@ -58,60 +58,10 @@ let localPlayerInitCount = [
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 ]
 
-function toggleHiddenParkLabels() {
-  switch (currentId) {
-    case '0':
-      parkLabels[0].classList.toggle('hidden');
-      break
-    case '1':
-      parkLabels[12].classList.toggle('hidden');
-      break
-    case '2':
-      parkLabels[10].classList.toggle('hidden');
-      break
-    case '3':
-      parkLabels[15].classList.toggle('hidden');
-      break
-    case '4':
-      parkLabels[3].classList.toggle('hidden');
-      break
-    case '5':
-      parkLabels[2].classList.toggle('hidden');
-      parkLabels[5].classList.toggle('hidden');
-      break
-    case '6':
-      parkLabels[13].classList.toggle('hidden');
-      parkLabels[14].classList.toggle('hidden');
-      break
-    case '7':
-      parkLabels[16].classList.toggle('hidden');
-      break
-    case '8':
-      parkLabels[1].classList.toggle('hidden');
-      break
-    case '9':
-      parkLabels[6].classList.toggle('hidden');
-      parkLabels[7].classList.toggle('hidden');
-      parkLabels[9].classList.toggle('hidden');
-      break
-    case '10':
-      parkLabels[11].classList.toggle('hidden');
-      break
-    case '11':
-      parkLabels[8].classList.toggle('hidden');
-      break
-    case '12':
-      parkLabels[4].classList.toggle('hidden');
-      break
-    default:
-      console.error('Invalid currentId value.')
-  }
-}
 function toggleStreamModal() {
   streamModal.classList.toggle('hidden');
   waBanner.classList.toggle('blur');
   mainSvg.classList.toggle('blur');
-  toggleHiddenParkLabels();
 }
 
 function onPlayerReady() {
@@ -135,6 +85,7 @@ function onPlayerReady() {
   }
   dorawa.setAttribute('listener', true);
 }
+
 dorawa.addEventListener('click', () => {
   if(!dorawa.getAttribute('listener')) {
     toggleStreamModal();
@@ -150,34 +101,29 @@ returnTarget.addEventListener('click', () => {
   });
 })
 
-
-function handleParkGroupClick() {
-  parkGroups.forEach(parkGroup => parkGroup.addEventListener('click', (e) => {
-    currentId = e.target.id.match(regexGetIdNumber)[0];
-      if (Number(currentId) >= 0 && Number(currentId) <= 16) {
-        toggleStreamModal();
-      }
-  }))
-}
-function handleParkLabelClick() {
-  parkLabels.forEach(parkLabel => parkLabel.addEventListener('click', (e) => {
+function handleParkGroupClick(e) {
+  currentId = e.target.id.match(regexGetIdNumber)[0];
+  if (Number(currentId) >= 0 && Number(currentId) <= 16) {
+    toggleStreamModal();
     currentLabelId = e.target.id.match(regexGetIdNumber)[0];
-      if(streams[currentLabelId].classList.contains('hidden')) {
-        streams[currentLabelId].classList.remove('hidden');
+  if(streams[currentLabelId].classList.contains('hidden')) {
+    streams[currentLabelId].classList.remove('hidden');
+  }
+  if (localPlayerInitCount[currentLabelId] === 0) {
+    players[currentLabelId] = new YT.Player(`stream${currentLabelId}`, {
+      events: {
+        'onReady': onPlayerReady,
       }
-      if (localPlayerInitCount[currentLabelId] === 0) {
-        players[currentLabelId] = new YT.Player(`stream${currentLabelId}`, {
-          events: {
-            'onReady': onPlayerReady,
-          }
-        })
-      }
-      if (localPlayerInitCount[currentLabelId] > 0) {
-        players[currentLabelId].playVideo();
-      }
-      localPlayerInitCount[currentLabelId] += 1;
-  }))
+    })
+  }
+  if (localPlayerInitCount[currentLabelId] > 0) {
+    players[currentLabelId].playVideo();
+  }
+  localPlayerInitCount[currentLabelId] += 1;
+  }
 }
 
-handleParkGroupClick();
-handleParkLabelClick();
+function addParkGroupClickListener() {
+  parkGroups.forEach(parkGroup => parkGroup.addEventListener('click', handleParkGroupClick))
+}
+addParkGroupClickListener();
