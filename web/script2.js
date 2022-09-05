f0f641255a404baf7301424e413c451fbc88013d
Gregory Monroe <36177342+gnjmonroe@users.noreply.github.com>
2022-09-05 10:25:20 +0900

    Added Airbnb style support via ESLint.

--- web/script2.js
+++ web/script2.js
@@ -1,34 +1,38 @@
-const htmlEl = document.getElementsByTagName('body');
-const parkGroups = Array.prototype.slice.call(document.getElementsByClassName('park'));
-const streams = Array.prototype.slice.call(document.getElementsByClassName('stream'));
-const streamModal = document.getElementById('streamModal');
+const htmlEl = document.getElementsByTagName("body");
+const parkGroups = Array.prototype.slice.call(
+  document.getElementsByClassName("park")
+);
+const streams = Array.prototype.slice.call(
+  document.getElementsByClassName("stream")
+);
+const streamModal = document.getElementById("streamModal");
 
 let currentId;
 var players = [...Array(streams.length)];
 let localPlayerInitCount = new Array(players.length).fill(0);
 
-var tag = document.createElement('script');
+var tag = document.createElement("script");
 tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
+var firstScriptTag = document.getElementsByTagName("script")[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 function toggleStreamModal() {
-  streamModal.classList.toggle('hidden');
-  if (document.body.style.overflow === 'hidden') {
-    document.body.style.overflow = 'visible';
-  } else if (document.body.style.overflow = 'visible') {
-    document.body.style.overflow = 'hidden';
+  streamModal.classList.toggle("hidden");
+  if (document.body.style.overflow === "hidden") {
+    document.body.style.overflow = "visible";
+  } else if ((document.body.style.overflow = "visible")) {
+    document.body.style.overflow = "hidden";
   }
 }
 
 function onPlayerReady() {
   players[currentId].playVideo();
-  if(!dorawa.getAttribute('listener')) {
-    dorawa.addEventListener('click', () => {
+  if (!dorawa.getAttribute("listener")) {
+    dorawa.addEventListener("click", () => {
       toggleStreamModal();
-      streams.forEach(stream => {
-        if (!stream.classList.contains('hidden')) {
-          stream.classList.add('hidden');
+      streams.forEach((stream) => {
+        if (!stream.classList.contains("hidden")) {
+          stream.classList.add("hidden");
         }
       });
       try {
@@ -37,35 +41,36 @@ function onPlayerReady() {
         console.log(players[currentId]);
         console.error(error);
       }
-    }
-    )
+    });
   }
-  dorawa.setAttribute('listener', true);
-};
+  dorawa.setAttribute("listener", true);
+}
 
 function handleParkGroupClick(e) {
   currentId = e.target.dataset.parkGroup;
   toggleStreamModal();
-  if (streams[currentId].classList.contains('hidden')) {
-    streams[currentId].classList.remove('hidden');
+  if (streams[currentId].classList.contains("hidden")) {
+    streams[currentId].classList.remove("hidden");
   }
-  if(localPlayerInitCount[currentId] === 0) {
+  if (localPlayerInitCount[currentId] === 0) {
     players[currentId] = new YT.Player(`stream${currentId}`, {
       events: {
-        'onReady': onPlayerReady,
-      }
+        onReady: onPlayerReady,
+      },
     });
   }
   if (localPlayerInitCount[currentId] > 0) {
     players[currentId].playVideo();
   }
   localPlayerInitCount[currentId] += 1;
-};
+}
 
-dorawa.addEventListener('click', () => {
-  if(!dorawa.getAttribute('listener')) {
+dorawa.addEventListener("click", () => {
+  if (!dorawa.getAttribute("listener")) {
     toggleStreamModal();
   }
 });
 
-parkGroups.forEach(parkGroup => parkGroup.addEventListener('click', handleParkGroupClick));
+parkGroups.forEach((parkGroup) =>
+  parkGroup.addEventListener("click", handleParkGroupClick)
+);
