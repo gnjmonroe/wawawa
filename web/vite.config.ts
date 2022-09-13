89e01cfa5c6b0f2ec08be3775caa53019a92d52a
Greg Monroe <gnjmonroe@gmail.com>
2022-09-13 15:29:04 +0900

    Removed postcss plugin. Too complicated for now.

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import postcss from 'rollup-plugin-postcss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    postcss({
-      modules: {
-        generateScopedName: '[hash:base64:8]',
-      },
-      autoModules: true,
-    }),
   ],
 });
