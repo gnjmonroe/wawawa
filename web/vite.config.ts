5b4d9058ec445855d6c34a829c4ce126e03ab027
Greg Monroe <gnjmonroe@gmail.com>
2022-09-13 15:15:32 +0900

    Testing postcss-plugin-rollup to minify classnames.

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import postcss from 'rollup-plugin-postcss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    postcss({
+      modules: {
+        generateScopedName: '[hash:base64:8]',
+      },
+      autoModules: true,
+    }),
+  ],
 });
