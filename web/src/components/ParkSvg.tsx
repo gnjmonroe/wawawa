36c44fe152c045debc51d579986415bcdeccad29
Greg Monroe <gnjmonroe@gmail.com>
2022-09-13 14:11:48 +0900

    Hotfix attempt via modifying filepaths.

--- web/src/components/ParkSvg.tsx
+++ web/src/components/ParkSvg.tsx
@@ -7,7 +7,7 @@ export default function ParkSvg(props: { parkGroup: number }) {
     <svg className="bg-svg" width="240" height="135" viewBox="0 0 240 135" data-park-group={parkGroup} xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
       <defs>
         <pattern id={`bg-pattern-${parkGroup}`} patternUnits="userSpaceOnUse" x="0" y="0" width="240" height="135">
-          <image href={`./assets/park-${parkGroup}.png`} x="0" y="0" width="240" height="135" />
+          <image href={`assets/park-${parkGroup}.png`} x="0" y="0" width="240" height="135" />
         </pattern>
       </defs>
       <path className="bg-path" data-park-group={parkGroup} d="M100,0 L60,40 L60,120 L140,120 L180,80 L180,0z" />
